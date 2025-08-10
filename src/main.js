fd9912fe927c01396c03fd6ec70f7a9568cdbbcb
AfonsoCalinas <calinaspentas@gmail.com>
2025-08-10 20:18:05 +0100

    fix: Final Fix

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-import * as THREE from 'three';
-import { gsap } from "gsap";
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.152.0/build/three.module.js';
+import { gsap } from "https://cdn.jsdelivr.net/npm/gsap@3.12.2/index.js";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
